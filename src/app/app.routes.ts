e44253a425ca526c360fac91b8e97e9f38f40a0c
Gugugaga <aderifkysabian666@gmail.com>
2025-11-17 14:55:16 +0700

    Data Mahasiswa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Dashboard } from './dashboard/dashboard';
 import { Login } from './login/login';
 import { Dashboard2 } from './dashboard2/dashboard2';
 import { Dashboard3 } from './dashboard3/dashboard3';
+import { Mahasiswa } from './mahasiswa/mahasiswa';
 
 export const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: "dashboard", component: Dashboard },
   { path: "dashboard2", component: Dashboard2 },
   { path: "dashboard3", component: Dashboard3 },
+  { path: "mahasiswa", component: Mahasiswa },
   { path:"login", component: Login }
 ];
